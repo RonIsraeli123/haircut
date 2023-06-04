d94cb756552ffc15b2141daec07f880f251b6592
superseniordev <stamdvarim1000@gmail.com>
2023-06-04 15:49:59 +0300

    add tors

--- src/components/Pages/Tors/Tors.js
+++ src/components/Pages/Tors/Tors.js
@@ -3,25 +3,32 @@ import React from 'react';
 import Container from '@mui/material/Container';
 
 import TorView from './TorView';
+const emptyTorsMsg = 'לא נקבעו תורים';
 
 const UserTor = (props) => {
   return (
     <div className='appointment-section'>
       <Container component='main' maxWidth='xs'>
-        {props.userTors.map((appointmentDetails) => {
-          return (
-            <TorView
-              key={appointmentDetails['id']}
-              day={appointmentDetails['day']}
-              hour={appointmentDetails['hour']}
-              jobType={appointmentDetails['jobType']}
-              barberName={appointmentDetails['barberName']}
-              id={appointmentDetails['id']}
-              userTors={props.userTors}
-              setUserTors={props.setUserTors}
-            />
-          );
-        })}
+        {props.userTors ? (
+          <div>
+            {props.userTors.map((appointmentDetails) => {
+              return (
+                <TorView
+                  key={appointmentDetails['id']}
+                  day={appointmentDetails['day']}
+                  hour={appointmentDetails['hour']}
+                  jobType={appointmentDetails['jobType']}
+                  barberName={appointmentDetails['barberName']}
+                  id={appointmentDetails['id']}
+                  userTors={props.userTors}
+                  setUserTors={props.setUserTors}
+                />
+              );
+            })}
+          </div>
+        ) : (
+          <div> {emptyTorsMsg}</div>
+        )}
       </Container>
     </div>
   );
